c8793e0fcbfa187d704fbea7c9a29adc46f9412d
raselworshop <raselworshop@gmail.com>
2024-10-23 09:28:52 -0400

    currently cooking table and functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,57 @@ import { useState } from 'react'
 
 function App() {
   const [reciepeQueue, setReciepeQueue] = useState([]);
-   
-  const handleToCook = reciepe =>{
+  const [preparedRecipes, setPreparedRecipes] = useState([]);
+  const [totlTime, setTotalTime] = useState(0)
+  const [totlCalories, setTotalCalories] = useState(0)
+
+  const handleToCook = reciepe => {
     // console.log(reciepe, 'calling here')
-    if(!reciepeQueue.some(r=> r.recipe_id === reciepe.recipe_id)){
+    if (!reciepeQueue.some(r => r.recipe_id === reciepe.recipe_id)) {
       setReciepeQueue([...reciepeQueue, reciepe])
-    }else{
+    } else {
       alert('Recipe is already in Queue!!!')
     }
   }
-  console.log(reciepeQueue)
+  // console.log(reciepeQueue)
+
+  const handleRemoveRecipes = id => {
+    // find which recipe will remove
+    const deletedRecipes = reciepeQueue.find(reciepe => reciepe.recipe_id === id)
+
+    // remove from want to cook table
+    const updatedQueue = reciepeQueue.filter(recipe => recipe.recipe_id !== id)
+    setReciepeQueue(updatedQueue);
+    setPreparedRecipes([...preparedRecipes, deletedRecipes])
+  }
+
+  const calculateTimeAndCalories = (time, calories) => {
+    setTotalTime(parseFloat(totlTime) + parseFloat(time))
+    setTotalCalories(parseFloat(totlCalories) + parseFloat(calories))
+  }
 
   return (
     <>
-        {/* header section */}
-        <Header></Header>
-        {/* banner section */}
-        <Banner></Banner>
-        {/* reciepes section */}
-        <Reciepes></Reciepes>
-        {/* reciepes card section */}
-        <section className='flex flex-col md:flex-row gap-6 sm:max-w-2xl md:max-w-4xl lg:max-w-7xl mx-auto'>
-          {/* cards */}
-            <ReciepesCard handleToCook={handleToCook}></ReciepesCard>
-          {/* side bar */}
-            <Sidebar reciepeQueue={reciepeQueue}></Sidebar>
-        </section>
+      {/* header section */}
+      <Header></Header>
+      {/* banner section */}
+      <Banner></Banner>
+      {/* reciepes section */}
+      <Reciepes></Reciepes>
+      {/* reciepes card section */}
+      <section className='flex flex-col md:flex-row gap-6 sm:max-w-2xl md:max-w-4xl lg:max-w-7xl mx-auto'>
+        {/* cards */}
+        <ReciepesCard handleToCook={handleToCook}></ReciepesCard>
+        {/* side bar */}
+        <Sidebar
+          totlCalories={totlCalories}
+          totlTime={totlTime}
+          calculateTimeAndCalories={calculateTimeAndCalories}
+          preparedRecipes={preparedRecipes}
+          handleRemoveRecipes={handleRemoveRecipes}
+          reciepeQueue={reciepeQueue}
+        ></Sidebar>
+      </section>
     </>
   )
 }
--- src/Components/Functional-component/Sidebar.jsx
+++ src/Components/Functional-component/Sidebar.jsx
@@ -1,5 +1,5 @@
 
-const Sidebar = ({ reciepeQueue }) => {
+const Sidebar = ({ reciepeQueue, handleRemoveRecipes, preparedRecipes, calculateTimeAndCalories, totlTime, totlCalories }) => {
     return (
         <div className="md:w-1/3 border-2 rounded-2xl text-gray-600 p-2 bg-base-100">
             {/* want to cook table */}
@@ -24,8 +24,10 @@ const Sidebar = ({ reciepeQueue }) => {
                                 <td>{recipe.preparation_time}</td>
                                 <td>{recipe.calories}</td>
                                 <td>
-                                    <button onClick={
-                                        () => console.log("cooker handle clicked")}
+                                    <button onClick={() =>{ 
+                                                handleRemoveRecipes(recipe.recipe_id)
+                                                calculateTimeAndCalories(recipe.preparation_time, recipe.calories)
+                                            }}
                                         className="btn btn-sm bg-emerald-400 rounded-full px-2 md:px-3 my-2 text-gray-800 font-bold">
                                         Preparing
                                     </button>
@@ -37,6 +39,36 @@ const Sidebar = ({ reciepeQueue }) => {
             </div>
 
             {/* Currently Cooking Table */}
+            <div className="overflow-x-auto mt-8">
+                <h3 className="text-xl font-semibold text-center border-b-2 pb-3">Currently Cooking: {preparedRecipes.length}</h3>
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Time</th>
+                            <th>Calories</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {preparedRecipes.map((recipe, index) => (
+                            <tr className="hover hover:rounded-xl" key={index}>
+                                <th>{index + 1}</th>
+                                <td>{recipe.recipe_name}</td>
+                                <td>{recipe.preparation_time}</td>
+                                <td>{recipe.calories}</td>
+                            </tr>
+                        ))}
+                        <tr>
+                            <th></th>
+                            <td></td>
+                            <td>Total Time = {totlTime} min.</td>
+                            <td>Total Calories = {totlCalories} Cal.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
